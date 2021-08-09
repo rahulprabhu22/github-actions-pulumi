2842dce068b97c2bdda73b49dd2a0c892e8bef84
rahulprabhu22 <rprabhu@presidio.com>
2021-08-09 12:37:54 +0530

    Dev Commit

--- serverless/github-actions/lambda.py
+++ serverless/github-actions/lambda.py
@@ -12,6 +12,6 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hello from Lambda! Hello')
     }
 
